dffa8f322d82506ea2c0c5d6423b8d296ee6bf24
venumadhav <venumadhav.velagaleti@gmail.com>
2024-11-04 16:31:24 +0530

    input using scanner class

---
+++ Java_Basics/input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Name");
+        String name=sc.nextLine();
+        System.out.println(name);
+        sc.close();
+    }
+}
